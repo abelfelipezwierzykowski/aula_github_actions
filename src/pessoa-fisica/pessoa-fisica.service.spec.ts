efdc20bdc7eac39038c61f8bfa38a175d1568d65
abelfelipezwierzykowski <aga-abelzwierzykowski@ugv.edu.br>
2026-09-03 09:56:30 -0300

    fix: update valid CPF value in PessoaFisicaService tests

--- src/pessoa-fisica/pessoa-fisica.service.spec.ts
+++ src/pessoa-fisica/pessoa-fisica.service.spec.ts
@@ -3,7 +3,7 @@ import { PessoaFisicaService } from './pessoa-fisica.service';
 describe('PessoaFisicaService', () => {
     const service = new PessoaFisicaService();
 
-    const cpfValido = '5299822472';
+    const cpfValido = '03051153087';
 
     const dadosValidos = {
         nome: 'Maria Oliveira',
